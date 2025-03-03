dd0a34f947ecaca6dd5aea75a6dc8afecb6d4df7
Indian1999 <haszab99@gmail.com>
2025-03-03 16:18:23 +0100

    Prog tételek mátrixokra

--- 2. félév/5ora.py
+++ 2. félév/5ora.py
@@ -1,5 +1,5 @@
 import random
-
+import math
 # Mátrixok (táblázat)
 # Olyan listák amik listákat tárolnak
 
@@ -42,13 +42,32 @@ for row in mtx:
     print(row)
     
 # 1. feladat: Határozzuk meg a mátrix elemeinek az összegét:
-
+összeg = 0
+for i in range(len(mtx)):
+    for j in range(len(mtx[i])):
+        összeg += mtx[i][j]
+print("Összeg:", összeg)
 # 2. feladat: Határozzuk meg, hogy melyik elem a legkisebb a mátrixban:
-
+minimum = mtx[0][0]
+for i in range(len(mtx)):
+    for j in range(len(mtx[i])):
+        if mtx[i][j] < minimum:
+            minimum = mtx[i][j]
+print("Legkisebb elem:", minimum)
 # 3. feladat: Határozzuk meg, hogy melyik elem a legnagyobb a mátrixban:
-
+maximum = mtx[0][0]
+for i in range(len(mtx)):
+    for j in range(len(mtx[i])):
+        if mtx[i][j] > maximum:
+            maximum = mtx[i][j]
+print("Legnagyobb elem:", maximum)
 # 4. feladat: Határozzuk meg, a mátrix elemeinek az átlagát
-
+elemek_száma = len(mtx) * len(mtx[0])
+átlag = összeg / elemek_száma
+print("Az elemek átlaga:", round(átlag,2))
 # 5. feladat: Módosítsuk a mátrix elemeit úgy, hogy a negatív számokból csináljunk pozitívat
 # azaz, vegyük a számok abszolút értékét (pl.: -5 -> 5)
+mtx = [[int(math.fabs(mtx[i][j])) for j in range(len(mtx[i]))] for i in range(len(mtx))]
+for row in mtx:
+    print(row)
 
