6934b8ca2974f84a1eed9c493f83637e615fa5b3
Indian1999 <haszab99@gmail.com>
2025-05-26 16:02:25 +0200

    nan értékek törlése

---
+++ 2. félév/12ora/12ora.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("2. félév/12ora/library.csv")
+data = data.drop(columns = ["Edition Statement", "Contributors", 'Corporate Author', 'Corporate Contributors', 'Former owner', 'Engraver', 'Issuance type', 'Flickr URL', 'Shelfmarks'])
+
+print("Identifier is unique:", data["Identifier"].is_unique) # True -> használhajtuk inexként (kulcsként)
+print("Place of Publication is unique:", data["Place of Publication"].is_unique) # False
+
+data = data.set_index("Identifier")
+
+# az extract függvény első paramétere egy reguláris kifejéz (egy minta [pattern])
+# Ennek a paraméternek kell megadni, hogy milyen mintára illeszkedő substringet (részszöveget)
+# szedjünk ki az oszlopból
+# Ezt a mintát egy reguláris kifejezéssel (regular expression [regex]) tudjuk leírni
+# ()    - ami a zárójelen belül van, azt kimentjük, vissza lesz adva a years df-be
+# \d    - digit, 1 számjegyet azonosít
+# {4}   - a \d után van -> 4 darab számjegy kell hogy egymás mellet legyen
+# Ha van a stringben 4 számjegy egymás mellett, akkor azt a négy számjegyet visszaadja
+years = data["Date of Publication"].str.extract(r"(\d{4})")
+print(years)
+print(years[0].isna().sum()) # 59 nan/null érték szerepel az oszlopan (59 könyvnek nem ismerjük a kiadási dátumát)
+"""
+A Nan értékek problémákhoz vezethetnek adatelemzésnél, ilyenkor választanunk kell egy módszert
+amivel ezeket lekezeljük
+"""
+"""
+1. módszer: Legyen -1 minden NaN érték
+# Azért, hogy intté tudjuk konvertálni, töltsük fel a nan értékeket egy számmal
+# Ha egy érték NaN, akkor legyen 9999, -1, -1000
+years[0] = years[0].fillna(-1)
+print(years[0].isna().sum()) # 0 -> feltöltöttók a nan értékeket -1, úgyhogy már nincs 1 nan se
+data["Date of Publication"] = years[0].astype(int)
+print(data.head())
+"""
+
+"""
+2. módszer: A NaN értékeket állítsuk be az oszlop átlag értékére
+average = round(pd.to_numeric(years[0]).mean()) # 1859
+print(average)
+years[0] = years[0].fillna(average)
+data["Date of Publication"] = years[0].astype(int)
+print(data.head())
+"""
+
+"""
+3. módszer: Töröljük a NaN értékeket
+"""
+data["Date of Publication"] = pd.to_numeric(years[0])
+data = data.dropna(subset=["Date of Publication"])
+print(data["Date of Publication"].isna().sum())
+data["Date of Publication"] = data["Date of Publication"].astype(int)
+print(data)
