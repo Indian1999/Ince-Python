58a19ea8e2f9f748ba8ab04a643fc6b146687ddb
indian1999 <haszab99@gmail.com>
2025-09-22 16:13:45 +0200

    User constructor

---
+++ 3. félév/feladatsor.py
@@ -0,0 +1,51 @@
+export = {
+ "users": [
+ { "_id" : "4ke83hsejs", "name" : "John", "role" : "developer"},
+ { "_id" : "34rkmlqek2", "name" : "Kevin", "role" : "developer"},
+ { "_id" : "83i2kkwwj3", "name" : "Joe", "role" : "developer"},
+ { "_id" : "12kw4jwmcq", "name" : "Bob", "role" : "developer"},
+ { "_id" : "4i5ii32313", "name" : "Robert", "role" : "developer"},
+ { "_id" : "lkr3jwl4k5", "name" : "Mary", "role" : "manager"},
+
+ ],
+ "sprints": [
+ { "_id": "2",
+ "tasks":[ { "_id" : "T0005", "storypoint" : 60, "task" : "Seach", "hours": [ ("4ke83hsejs", 23), ("83i2kkwwj3", 15), ("4i5ii32313", 11)]},
+ { "_id" : "T0006", "storypoint" : 20, "task" : "Filter",
+ "hours": [ ("4ke83hsejs", 17), ("4i5ii32313", 12)]},
+ { "_id" : "T0007", "storypoint" : 40, "task" : "List items",
+ "hours": [ ("4i5ii32313", 3)]},
+ ]
+ },{ "_id": "3",
+ "tasks":[ { "_id" : "T008", "storypoint" : 25, "task" : "Basket",
+ "hours": [ ("4ke83hsejs", 6), ("83i2kkwwj3", 15), ("4i5ii32313", 23)]},
+ { "_id" : "T0009", "storypoint" : 20, "task" : "Subscription",
+ "hours": [ ("4ke83hsejs", 3), ("83i2kkwwj3", 24), ("4i5ii32313", 23)]},
+ { "_id" : "T0010", "storypoint" : 35, "task" : "Order",
+ "hours": [ ("83i2kkwwj3", 13), ("4i5ii32313", 5)]}
+ ]
+ },{ "_id": "1",
+ "tasks":[ { "_id" : "T0001", "storypoint" : 50, "task" : "Login",
+ "hours": [ ("4ke83hsejs", 13), ("83i2kkwwj3", 5), ("4i5ii32313", 11)]},
+ { "_id" : "T0002", "storypoint" : 20, "task" : "Registration",
+ "hours": [ ("4ke83hsejs", 28), ("4i5ii32313", 12)]},
+ { "_id" : "T0003", "storypoint" : 30, "task" : "Menu",
+ "hours": [ ("83i2kkwwj3", 7), ("4i5ii32313", 5)]},
+ { "_id" : "T0004", "storypoint" : 40, "task" : "Design",
+ "hours": [ ("4ke83hsejs", 17)]}
+ ]
+ },
+ ]
+}
+
+# Minden osztályban legyen konstruktor (__init__())
+# Minden osztályon belüli függvény első paramétere a self
+class User:
+    def __init__(self, id, password, name, role):
+        self.id = id
+        self.password = password
+        self.name = name
+        self.role = role
+
+myUser = User("myId1", "12345", "John", "Developer")
+print(myUser.role)
