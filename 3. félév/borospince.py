6071fba243d19e769e22362933d0e8a6cfb79974
Indian1999 <haszab99@gmail.com>
2025-09-29 16:58:11 +0200

    Bor class

--- 3. félév/borospince.py
+++ 3. félév/borospince.py
@@ -1,1 +1,42 @@
 # Link: https://richardkorom.hu/feladatok/programozas/oop/#
+
+class BorospinceException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class Bor:
+    def __init__(self, evjarat, fajta, alkoholtartalom = 12.5):
+        self.evjarat = evjarat
+        self.fajta = fajta
+        self.alkoholtartalom = alkoholtartalom
+
+    def getEvjarat(self):
+        return self.evjarat
+    
+    def setEvjarat(self, ev):
+        self.evjarat = ev
+
+    def getFajta(self):
+        return self.fajta
+    
+    def setFajta(self, fajta):
+        self.fajta = fajta
+
+    def setAlkohol(self, alkohol):
+        if 0 > alkohol or 100 < alkohol:
+            raise BorospinceException("Az alkoholtartalomnak 0 és 100 között kell lennie!")
+        self.alkoholtartalom = alkohol
+
+    def __eq__(self, value):
+        if type(value) != Bor:
+            return False
+        if self.evjarat == value.evjarat and self.fajta.lower() == value.fajta.lower() and self.alkoholtartalom == value.alkoholtartalom:
+            return True
+        return False
+        
+
+bor1 = Bor(2000, "vörös")
+bor1.setEvjarat(2015)
+print(bor1.getEvjarat())
+    
