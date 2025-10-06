d7cd6978819d263d2ee1345a86a4337afcc64c35
Indian1999 <haszab99@gmail.com>
2025-10-06 16:44:55 +0200

    4-6. feladatok

--- 3. félév/otszaz.py
+++ 3. félév/otszaz.py
@@ -24,3 +24,37 @@ print()
 
 print("3. feladat")
 print(f"Az első vásárló {len(vasarlasok[0].termekek)} darab árucikket vásárolt.")
+print()
+
+print("4. feladat")
+sorszam = int(input("Adja meg a vásárlás sorszámát: ")) - 1
+arucikk = input("Adja meg az árucikk nevét: ")
+darabszam = int(input("Adja meg a vásárolt darabszámot: "))
+print()
+
+print("5. feladat")
+szamlalo = 0
+utolso_index = None
+elso_index = None
+for i in range(len(vasarlasok)):
+    if arucikk in vasarlasok[i].termekek:
+        szamlalo += 1
+        utolso_index = i
+        if szamlalo == 1:
+            elso_index = i
+print(f"Az első vásárlás sorszáma: {elso_index + 1}")
+print(f"Az utolsó vásárlás sorszáma: {utolso_index + 1}")
+print(f"{szamlalo} vásárlás során vettek belőle.")
+print()
+
+def ertek(db):
+    if db == 1:
+        return 500
+    elif db == 2:
+        return 950
+    else:
+        return 950 + 400 * (db - 2)
+    
+print("6. feladat")
+print(f"{darabszam} darab vételekor fizetendő: {ertek(darabszam)}")
+print()
