99df63ad3bdead385ff7740ab145129d04c7b22f
Indian1999 <haszab99@gmail.com>
2025-10-06 16:16:30 +0200

    3. feladat

--- 3. félév/otszaz.py
+++ 3. félév/otszaz.py
@@ -1,18 +1,18 @@
 class Vasarlas:
     def __init__(self, lista:list = []):
-        self.termekek = lista
+        self.termekek = lista.copy()
 
 def beolvasas() -> list[Vasarlas]:
     with open("3. félév/penztar.txt", "r", encoding="utf-8") as f:
         vasarlasok = []
-        aktualis_vasarlas = Vasarlas()
+        aktualis_vasarlas = []
         for line in f:
             line = line.strip()
             if line != 'F':
-                aktualis_vasarlas.termekek.append(line)
+                aktualis_vasarlas.append(line)
             else:
-                vasarlasok.append(aktualis_vasarlas)
-                aktualis_vasarlas = Vasarlas()
+                vasarlasok.append(Vasarlas(aktualis_vasarlas))
+                aktualis_vasarlas = []
         return vasarlasok
     
 vasarlasok = beolvasas()
@@ -20,4 +20,7 @@ vasarlasok = beolvasas()
 # 2. feladat:
 print("2. feladat")
 print("A fizetések száma:", len(vasarlasok))
+print()
 
+print("3. feladat")
+print(f"Az első vásárló {len(vasarlasok[0].termekek)} darab árucikket vásárolt.")
