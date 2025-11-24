1c946fa3b0f3c7eaeb152028252ac834e62980c6
Indian1999 <haszab99@gmail.com>
2025-11-24 15:48:19 +0100

    gyakorlo feladatok

---
+++ 3. félév/vizsgara_keszules/gyakorlas.py
@@ -0,0 +1,19 @@
+# 1. Készíts egy listát az első 20 négyzetszámmal list comprehensionnel.
+
+# 2. Szűrd ki egy 1-100 közötti lista elemei közül azokat, amelyek oszthatók 3-mal.
+
+# 3. Írj függvényt, amely két számot kap és visszaadja az első szám második hatványon (** operátor használatával).
+
+# 4. Írj függvényt, amely egy lista összegét adja vissza beépített sum nélkül.
+
+# 5. Írj rekurzív függvényt, amely kiszámítja n faktoriálisát.
+
+# 6. Írj rekurzív függvényt, amely megszámolja, hány szám van egy listában.
+
+# 7. Írd ki a 10-től 0-ig visszaszámlálást range segítségével.
+
+# 8. Készíts függvényt, amely két listát összefűz egy új listába, a + operátor használata nélkül.
+
+# 9. List comprehKészíts listát, amely egy adott szöveg összes karakterének ASCII kódját tartalmazza.
+
+# 10. Kombinált fAdj meg egy listát 1-20-ig, majd list comprehensionmel készíts új listát, amelyben minden szám négyzete szerepel, de csak a páros számoké.
