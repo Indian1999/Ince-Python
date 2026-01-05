be75cd6fd1911db418288979f8382e2ab492967f
Indian1999 <haszab99@gmail.com>
2026-01-05 15:48:33 +0100

    konyvek feladat

--- 3. félév/vizsgara_keszules/dictionaries.py
+++ 3. félév/vizsgara_keszules/dictionaries.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 def alapok(): # ctrl + alt + lefele nyíl
     ember = {
@@ -30,9 +31,43 @@ def alapok(): # ctrl + alt + lefele nyíl
     for key, value in ember.items():
         print(key, value)
 
-path = os.path.dirname(__file__)
-path = os.path.join(path, "termekek.txt") 
-#print(path) 
+def termekek_feladat():
+    path = os.path.dirname(__file__)
+    path = os.path.join(path, "termekek.txt") 
+    #print(path) 
 
-with open(path, "r", encoding="utf-8") as f:
-    pass
+    with open(path, "r", encoding="utf-8") as f:
+        data = f.read().split("\n")
+
+    termek_count = {}
+    for termek in data:
+        if termek in termek_count.keys():
+            termek_count[termek] += 1
+        else:
+            termek_count[termek] = 1
+
+    for item in termek_count.items():
+        print(item)
+
+    termekek_rendezett = list(termek_count.items())
+    termekek_rendezett.sort(key = lambda x: x[1], reverse=True)
+    termekek_rendezett = dict(termekek_rendezett)
+    print(termekek_rendezett)
+
+random.seed(42)
+emberek = []
+for i in range(50): # 50 ember lesz
+    ember = {
+        "money": random.randint(100, 55000),
+        "age": random.randint(18, 90),
+        "profession": random.choice(["Programmer", "Receptionist", "Doctor", "Vet", "Dentist", "Telemarketer", "Carpenter", "Electrician", "Unemployed", "Engineer"])
+    }
+    emberek.append(ember)
+
+# 1. feladat: Mennyi az összes ember átlagos életkora?
+
+# 2. feladat: Mennyi csak a Programmer -ek áltlagos életkora
+
+# 3. feladat: Hány Receptionist van aki 30000 fölötti pénzzel rendelkezik?
+
+# 4. feladat: Mennyi pénzük van a Doctor, Vet és Dentist embereknek összesen együttvéve
